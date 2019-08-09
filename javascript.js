e862ba2afa242e725e3b95d860cae41581ad2599
Kelson <kelsoncarmozine@globo.com>
2019-08-09 11:11:21 -0300

    Ocultar input depois do envio do email

--- javascript.js
+++ javascript.js
@@ -4,4 +4,6 @@ function Alerta() {
     alert("O email "+ email +" foi enviado");
 
     document.getElementById("bv").innerHTML = "Bem vindo "+ email;
+    document.getElementById("butt").style.display = "none";
+    document.getElementById("inp").style.display = 'none';
 }
