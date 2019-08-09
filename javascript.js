e7aabc81b09aa7071c5b2b52d3fe5d8eb85f94dc
Kelson <kelsoncarmozine@globo.com>
2019-08-09 16:35:56 -0300

    Final

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 var email = '';
 function Alerta() {
-    email = document.getElementById('inp').value; 
-    vali =  /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
-    if (vali.test(email)) {
+    email = document.getElementById('inp').value;
+    var test = email.indexOf('@');
+    var test2 = email.indexOf('.');
+    //var vali =  /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+    //if (vali.test(email)) {
+
+    if ((test != -1) && (test2 > test)) {
         alert("O email "+ email +" foi enviado");
         document.getElementById("bv").innerHTML = "Bem vindo "+ email;
         document.getElementById("butt").style.display = "none";
