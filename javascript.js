4001f1078c35d8ccf559c2eaeab8583ed6986556
Kelson <kelsoncarmozine@globo.com>
2019-08-09 10:54:17 -0300

    Inclusão da mensagem de boas vindas

--- javascript.js
+++ javascript.js
@@ -2,4 +2,6 @@ var email = '';
 function Alerta() {
     email = document.getElementById('inp').value; 
     alert("O email "+ email +" foi enviado");
+
+    document.getElementById("bv").innerHTML = "Bem vindo "+ email;
 }
