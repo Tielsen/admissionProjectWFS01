67a25e18a9097dff61151517d6bd8cee6ef9743d
Kelson <kelsoncarmozine@globo.com>
2019-08-09 11:33:20 -0300

    Validação do email

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 var email = '';
 function Alerta() {
     email = document.getElementById('inp').value; 
-    alert("O email "+ email +" foi enviado");
-
-    document.getElementById("bv").innerHTML = "Bem vindo "+ email;
-    document.getElementById("butt").style.display = "none";
-    document.getElementById("inp").style.display = 'none';
+    vali =  /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+    if (vali.test(email)) {
+        alert("O email "+ email +" foi enviado");
+        document.getElementById("bv").innerHTML = "Bem vindo "+ email;
+        document.getElementById("butt").style.display = "none";
+        document.getElementById("inp").style.display = 'none';  
+    } 
+    else {
+        alert("Email invalido");
+    }
+    
 }
